65492154004cc990f8acd374508a25d09dd9ad95
Terrell Lemons <lemonsterrell43@gmail.com>
2020-05-05 01:20:37 -0700

    Day 2, complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('horses/', views.horses_index, name='horses_index'),
+  path('horses/<int:horse_id>/', views.horses_detail, name='horses_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-class Horse:  
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-horses = [
-  Horse('Lolo', 'tabby', 'foul little demon', 3),
-  Horse('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Horse('Raven', 'black tripod', '3 legged horse', 4)
-]
+from .models import Horse
 
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def horses_index(request):
+  horses = Horse.objects.all()
   return render(request, 'horses/index.html', { 
     'horses': horses, 
+  })
+
+def horses_detail(request, horse_id):
+  horse = Horse.objects.get(id=horse_id)
+  return render(request, 'horses/detail.html', { 
+    'horse': horse 
   })
