c0f693cfd1e8086191a8d35e9f0924685eab6f5c
Terrell Lemons <lemonsterrell43@gmail.com>
2020-05-05 02:53:13 -0700

    Day 4, completed one-to-many-model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+# A tuple of 2-tuples
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 # Create your models here.
 class Horse(models.Model):
   name = models.CharField(max_length=100)
@@ -13,3 +20,20 @@ class Horse(models.Model):
 
   def get_absolute_url(self):
     return reverse('horses_detail', kwargs={'pk': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+
+    # add the 'choices' field option
+    choices=MEALS,
+
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0],
+  )
+
+  horse = models.ForeignKey(Horse, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"  
