e5b12d234ecaceccd75da5484844449c5bfc9096
Terrell Lemons <lemonsterrell43@gmail.com>
2020-05-05 01:49:17 -0700

    Day 2, complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('horses/', views.horses_index, name='horses_index'),
-  path('horses/<int:horse_id>/', views.horses_detail, name='horses_detail'),
+  path('horses/', views.horses_index, name='index'),
+  path('horses/<int:horse_id>/', views.horses_detail, name='detail'),
 ]
